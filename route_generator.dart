d55b9f8edd22918665be205b9d974513ffefc446
Vipen_Kumar <47458626+12345vipen@users.noreply.github.com>
2021-05-22 14:45:42 +0530

    Update route_generator.dart

--- route_generator.dart
+++ route_generator.dart
@@ -1,17 +1,7 @@
 import 'package:dukan_app/screens/chat_screen.dart';
-import 'package:dukan_app/screens/detail_screen.dart';
-import 'package:dukan_app/screens/khata_screen.dart';
-import 'package:dukan_app/screens/medDetail_screen.dart';
-import 'package:dukan_app/screens/medId_screen.dart';
-import 'package:dukan_app/screens_sell_buy/buy_screen.dart';
-import 'package:dukan_app/screens_sell_buy/order_placed_screen.dart';
-import 'package:dukan_app/screens_sell_buy/sell_screen.dart';
-import 'package:dukan_app/screens_sell_buy/tab_screen.dart';
+import 'package:dukan_app/screens/order_placed_screen.dart';
+import 'package:dukan_app/screens/tab_screen.dart';
 import 'package:dukan_app/widgets/image/image_capture.dart';
-import 'package:dukan_app/widgets/khata/add_detail.dart';
-import 'package:dukan_app/widgets/khata/add_khata.dart';
-import 'package:dukan_app/widgets/med/addMed_detail.dart';
-import 'package:dukan_app/widgets/med/create_id.dart';
 import 'package:dukan_app/widgets/sell/sell.dart';
 import 'package:flutter/material.dart';
 
@@ -22,21 +12,7 @@ class RouteGenerator {
 
     switch (settings.name) {
       case '/':
-        return MaterialPageRoute(builder: (_) => KhataScreen());
-      case '/AddKhata':
-        return MaterialPageRoute(builder: (_) => AddKhata());
-      case '/DetailScreen':
-        return MaterialPageRoute(builder: (_) => DetailScreen(args));
-      case '/AddDetail':
-        return MaterialPageRoute(builder: (_) => AddDetail(args));
-      case '/MedIdScreen':
-        return MaterialPageRoute(builder: (_) => MedIdScreen());
-      case '/CreateId':
-        return MaterialPageRoute(builder: (_) => CreateId());
-      case '/MedDetailScreen':
-        return MaterialPageRoute(builder: (_) => MedDetailScreen(args));
-      case '/AddMedDetail':
-        return MaterialPageRoute(builder: (_) => AddMedDetail(args));
+        return MaterialPageRoute(builder: (_) => TabsScreen());
       case '/TabsScreen':
         return MaterialPageRoute(builder: (_) => TabsScreen());
       case '/Sell':
